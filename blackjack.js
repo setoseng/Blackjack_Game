d1c911473ca7040993630122ced382da35ce8222
setoseng <setoseng@gmail.com>
2017-05-28 13:03:23 -0500

    Fully Function Blackjack

--- blackjack.js
+++ blackjack.js
@@ -2,28 +2,118 @@
 $(document).ready(function(){
   $('#hit-button').hide();
   $('#stand-button').hide();
+  $('#restart-button').hide();
   //deal button
   $('#deal-button').click(function(){
+    playerHand.addCard(deck.draw());
+    playerHand.addCard(deck.draw());
+    dealerHand.addCard(deck.draw());
+    dealerHand.addCard(deck.draw());
 
-    var img1 = getCardImageUrl(playerHand[0]);
-    var img2 = getCardImageUrl(playerHand[1]);
+    var playerPoint = playerHand.getPoints();
+    var dealerPoint = dealerHand.getPoints();
 
-    var img3 = getCardImageUrl(dealerHand[0]);
-    var img4 = getCardImageUrl(dealerHand[1]);
+    var img1 = playerHand.hand[0].getImgUrl();
+
+    var img2 = playerHand.hand[1].getImgUrl();
+
+    var img3 = dealerHand.hand[0].getImgUrl();
+    var img4 = dealerHand.hand[1].getImgUrl();
 
     $('#player-hand').prepend(`<img src="${img1}" height="100" width="75"/>`);
     $('#player-hand').prepend(`<img src="${img2}" height="100" width="75"/>`);
 
     $('#dealer-hand').prepend(`<img src="${img3}" height="100" width="75"/> `);
     $('#dealer-hand').prepend(`<img src="${img4}" height="100" width="75"/> `);
+
     $('#deal-button').hide();
     $('#hit-button').show();
     $('#stand-button').show();
+    $('#player-points').html(`${playerPoint}`);
   });
   //hit button
   $('#hit-button').click(function(){
-    $('#player-hand').prepend('<img src="images/jack_of_hearts.png" height="100" width="75"/>' );
+    var card = deck.draw();
+    playerHand.addCard(card);
+    var img = card.getImgUrl();
+    playerPoint = playerHand.getPoints();
+    dealerPoint = dealerHand.getPoints();
+    $('#player-hand').prepend('<img src="' + img + '" height="100" width="75"/>' );
+    $('#player-points').html(`${playerPoint}`);
+    if (playerPoint>21){
+      $('#hit-button').hide();
+      $('#stand-button').hide();
+      //Bust
+      $('#dealer-points').html(`${dealerPoint}`);
+      $('#messages').html("Bust")
+      $('#restart-button').show();
+    }
+    //dealer also Draw
+
+      //hit button end
   });
+    //stand button
+    $('#stand-button').click(function(){
+      $('#hit-button').hide();
+      $('#stand-button').hide();
+      dealerPoint = dealerHand.getPoints();
+      playerPoint = playerHand.getPoints();
+      $('#dealer-points').html(`${dealerPoint}`);
+      while(dealerPoint<17){
+        card = deck.draw();
+        dealerHand.addCard(card);
+        img = card.getImgUrl();
+        dealerPoint = dealerHand.getPoints();
+        $('#dealer-hand').prepend('<img src="' + img + '" height="100" width="75"/>' );
+        $('#dealer-points').html(`${dealerPoint}`);
+        if (dealerPoint>21){
+          $('#messages').html("You win!");
+          $('#restart-button').show();
+        }
+      }
+      if (playerPoint>dealerPoint && playerPoint<=21){
+        $('#dealer-points').html(`${dealerPoint}`);
+        $('#messages').html("You win!");
+        $('#restart-button').show();
+      }
+      else if (dealerPoint>playerPoint && dealerPoint<=21){
+        $('#dealer-points').html(`${dealerPoint}`);
+        $('#messages').html("You lose.")
+        $('#restart-button').show();
+      }
+      else if (dealerPoint===playerPoint){
+        $('#dealer-points').html(`${dealerPoint}`);
+        $('#messages').html("Draw");
+        $('#restart-button').show();
+      }
+      else{
+        $('#messages').html("You win!");
+        $('#restart-button').show();
+      }
+    });//stand button end
+    //restart button
+    $('#restart-button').click(function(){
+      //clear html
+      $('#messages').html("");
+      $('#dealer-points').html("");
+      $('#player-points').html("");
+      $('#dealer-hand').html("");
+      $('#player-hand').html("");
+      //button hide/show
+      $('#hit-button').hide();
+      $('#stand-button').hide();
+      $('#restart-button').hide();
+      $('#deal-button').show();
+      //hand reset
+      playerHand = new Hand();
+      dealerHand = new Hand();
+
+      deck = new Deck();
+      card = new Card();
+      playerPoint = playerHand.getPoints();
+      dealerPoint = dealerHand.getPoints();
+
+    });//restart button end
 
 
 
--- functions.js
+++ functions.js
@@ -1,76 +1,78 @@
-var deck=[];
-var dealerHand=[];
-var playerHand=[];
-var card;
-var img=[];
-function newdeck(){
+
+//Card Constructor
+function Card (point,suit){
+  this.point = point;
+  this.suit = suit;
+} //Get Img Method
+Card.prototype.getImgUrl = function(){
+  if(this.point===1){
+    return ("images/ace_of_"+this.suit+".png");
+  }
+  else if (this.point===11) {
+      return ("images/jack_of_"+this.suit+".png");
+  }
+  else if (this.point===12) {
+      return ("images/queen_of_"+this.suit+".png");
+  }
+  else if (this.point===13) {
+      return ("images/king_of_"+this.suit+".png");
+  }
+
+  return ("images/"+this.point+"_of_"+this.suit+".png");
+
+}
+// Deck Constructor
+function Deck(){
+  this.deck = [];
   for (var x=1;x<=13;x++){
-    deck.push({point: x,suit:'spades'});
-    deck.push({point: x,suit:'clubs'});
-    deck.push({point: x,suit:'diamonds'});
-    deck.push({point: x,suit:'hearts'});
+    this.deck.push(new Card(x,'spades'));
+    this.deck.push(new Card(x,'clubs'));
+    this.deck.push(new Card(x,'diamonds'));
+    this.deck.push(new Card(x,'hearts'));
+  }
+  //Shuffle while making new deck
+  for (var i = this.deck.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = this.deck[i];
+      this.deck[i] = this.deck[j];
+      this.deck[j] = temp;
   }
 }
+//Draw Method
+Deck.prototype.draw = function(){
+  return this.deck.pop();
+}
 
+
+//Hand Constructor
+function Hand(){
+  this.hand = [];
+}//Push Card Into Hand Method
+Hand.prototype.addCard = function(card){
+  this.hand.push(card);
+}
+//Calls Deck Draw and Hand Draw
 function deal(){
-  card=deck.pop();
-  playerHand.push(card);
-  card=deck.pop();
-  playerHand.push(card);
-
-  card=deck.pop();
-  dealerHand.push(card);
-  card=deck.pop();
-  dealerHand.push(card);
-  console.log(playerHand);
-  return playerHand;
-  return dealerHand;
-
-
+  hand.addCard(deck.draw());
 }
+//calculatePoints in Hand
+Hand.prototype.getPoints=function(){
+  var counter = 0;
+  for(var h = 0;h<this.hand.length;h++){
+    var point = this.hand[h].point;
+      if (point > 10) {
+      counter = counter + 10;
+      }
+      else if (point ==1){
+       counter = counter + 11;
+      } else{
+        counter=counter+point;
+      }
+  }
+    return counter ;
+  };
 
-function getCardImageUrl(card){
-  var points=card.point;
-  var suits = card.suit;
-  if(points==1){
-    points = 'ace';
-  }
-  else if (points==11) {
-      points='jack';
-  }
-  else if (points==12) {
-      points='queen';
-  }
-  else if (points==13) {
-      points='king';
-  }
-  var url = "images/"+points+"_of_"+suits + ".png";
-  return url;
-};
-
-function calculatePoints(card){
-  var points=card.point;
-  var suits = card.suit;
-  var handPoint = 0;
-  if(points=="ace"){
-    handPoint +=1;
-  }
-  else if(points=="jack"||points=="queen"||points=="king"){
-    handPoint +=10;
-  }
-  return handPoint;
-}
-
-function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array;
-}
-newdeck();
-deck = shuffleArray(deck);
-deal();
-handPoint = calculatePoints(playerHand);
+var deck=new Deck();
+var dealerHand= new Hand();
+var playerHand= new Hand();
+var card = new Card();
